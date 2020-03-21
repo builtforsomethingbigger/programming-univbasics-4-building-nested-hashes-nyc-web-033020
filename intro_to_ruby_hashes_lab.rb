af15445a697ef4985b4c8c212d3b7f16767beff2
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-21 02:34:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,12 @@
 def base_hash{
-  :railroads => {}
+  railroads => {}
   }
 end
 
 def monopoly_with_second_tier{
-  :railroads => {
-    :pieces => 4
+  railroads: {
+    pieces: 4,
+    names: {}
   }
   }
 
